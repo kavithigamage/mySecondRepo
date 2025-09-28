bf2df0b30751d280e56c3efc5d1f70af71daad84
Kavithi Gamage <kavithigamage42@gmail.com>
2025-09-28 14:22:14 +0000

    it can do it in codespace now

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@ int main(){
   cout<< "enter 2 numbers: "<< endl;
   cin >> num1 >> num2;
   int sum = num1 + num2;
-  cout << "sum: "<< sum;
-    
+  int division = num1 / num2;
+  cout << "sum: "<< sum<< endl;
+  cout << "division: "<< division<< endl;
   return 0;
 }
